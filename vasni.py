fd0f66c2797988295a76c5ec889a3925a85d77a4
Cesar Segura <cesar.segura@ucem.ac.cr>
2022-11-08 12:10:26 -0500

    Se agregó el número de Vasni

---
+++ vasni.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+reqUrl = "http://51.222.14.197:3020/lead"
+
+headersList = {
+ "Accept": "*/*",
+ "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+ "Content-Type": "application/json" 
+}
+
+payload = json.dumps({
+  "message": "Esta es una prueba graciosa.",
+  "phone": "50688667800"
+})
+
+response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+
+print(response.text)
