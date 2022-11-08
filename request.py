8e4b77dd07f3345157f91a38be14d452a1e1f8ef
Cesar Segura <cesar.segura@ucem.ac.cr>
2022-11-08 12:07:26 -0500

    Primeras Pruebas

---
+++ request.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+reqUrl = "http://51.222.14.197:3020/lead"
+
+headersList = {
+ "Accept": "*/*",
+ "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+ "Content-Type": "application/json" 
+}
+
+payload = json.dumps({
+  "message": "Quiero comer pollo",
+  "phone": "50661650000"
+})
+
+response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+
+print(response.text)
